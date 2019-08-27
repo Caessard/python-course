4a766dcc67619ec99702113f0850c09172b57df8
César Hansy Dueñas Barragán <cesarhansy@gmail.com>
2019-08-27 16:32:02 -0500

    Includin age and gretting by age

--- saludos.py
+++ saludos.py
@@ -1,5 +1,14 @@
 # -*- coding: utf-8 -*-
+def say_hello(age):
+    if age >= 18:
+        print('Hola señor')
+    else:
+        print('Hola niño')
 
 name = str(input('¿Cuál es tu nombre?'))
 
 print('Hola, ' + name + '!')
+
+age_input = int(input('¿Qué edad tiene?'))
+
+say_hello(age_input)
