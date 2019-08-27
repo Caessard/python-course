b949ff9f90b6710d184d2d65948587aacf7d345d
César Hansy Dueñas Barragán <cesarhansy@gmail.com>
2019-08-27 13:14:34 -0500

    File to generate a square

---
+++ generate-square.py
@@ -0,0 +1,16 @@
+import turtle
+
+#Create lines and turn 90 degrees
+
+window = turtle.Screen()
+cesar = turtle.Turtle()
+cesar.forward(50)
+cesar.left(90)
+cesar.forward(50)
+cesar.left(90)
+cesar.forward(50)
+cesar.left(90)
+cesar.forward(50)
+cesar.left(90)
+
+turtle.mainloop()
